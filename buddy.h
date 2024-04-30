6abd2b227ff1be67b2b94f2b710738ef1e6d9efc
aa876433 <53804936+aa876433@users.noreply.github.com>
2024-04-30 17:23:40 +0800

    Update buddy.h

--- buddy.h
+++ buddy.h
@@ -10,6 +10,6 @@
 void *buddy_new(uint32_t split, uint32_t buf_size, void *buf, uint32_t align);
 void *buddy_alloc(void *ctx, uint32_t size);
 void buddy_free(void *ctx, void *ptr);
-void buddy2_dump(void *ctx);
+void buddy_dump(void *ctx);
 
 #endif //BUDDY_H
